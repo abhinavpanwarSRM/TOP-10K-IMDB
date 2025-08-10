e06147f3f19f35090d35ac1ef9445d1323d8f1fa
Abhinav Panwar <95019283+ABHINAVpanwar@users.noreply.github.com>
2025-08-11 01:10:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -76,4 +76,4 @@ def search():
     return jsonify(results.to_dict(orient="records"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
